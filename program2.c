f32793dcc51f0c50e3087ad83caebe43e626e469
YuvrajPandey2008 <yuvrajpandey1206@gmail.com>
2026-09-23 15:15:12 +0530

    [T5] Fix merge conflict in main function
    Resolve merge conflict in program2.c

--- program2.c
+++ program2.c
@@ -3,9 +3,6 @@ void main()
 {
     printf("Hello, Git!\n");
     printf("This is my first time using git.\n");
-<<<<<<< HEAD
     printf("This will cause a merge conflict main.\n");
-=======
     printf("This will cause merge conflict feature-1.\n");
->>>>>>> feature-1
 }
