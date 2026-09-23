403b6c3cbc3622a6b3f86654c6204156b2fa71fe
YuvrajPandey2008 <yuvrajpandey1206@gmail.com>
2026-09-23 15:21:00 +0530

    [T6] merge conflict due to rebase resolved

--- program.c
+++ program.c
@@ -3,9 +3,6 @@ void main()
 {
     printf("Hello, World!\n");
     printf("This is my first time using git.\n");
-<<<<<<< HEAD
     printf("Merge conflict rebase main\n");
-=======
     printf("merge conflict feature-1.\n");
->>>>>>> 0a83b57 ([T6] editted program.c feature-1)
 }
