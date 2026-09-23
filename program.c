48563554afbb0765eacc57599da3595055998cdc
YuvrajPandey2008 <yuvrajpandey1206@gmail.com>
2026-09-23 15:42:05 +0530

    [T8] Squash merging a pull request and resolving conflicts
    * [T6] editted program.c feature-1
    
    * [T6] merge conflict due to rebase resolved
    
    * [T7] editted program2.c
    
    * [T7] change 2 in program.c
    
    * [T7] change 3 in program.c
    
    * [T7] squash merge rebase
    
    [T7] editted program2.c
    
    [T7] change 2 in program.c
    
    [T7] change 3 in program.c

--- program.c
+++ program.c
@@ -4,6 +4,7 @@ void main()
     printf("Hello, World!\n");
     printf("This is my first time using git.\n");
     printf("Merge conflict rebase main\n");
+    printf("merge conflict feature-1.\n");
     printf("Change 1\n");
     printf("Change 2\n");
     printf("Change 3\n");
