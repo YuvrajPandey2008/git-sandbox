57f82f49f189876ce4157a8cd33f4e21a4c46770
YuvrajPandey2008 <yuvrajpandey1206@gmail.com>
2026-09-23 15:19:03 +0530

    [T6] editted program.c feature-1

--- program.c
+++ program.c
@@ -3,5 +3,9 @@ void main()
 {
     printf("Hello, World!\n");
     printf("This is my first time using git.\n");
+<<<<<<< HEAD
     printf("Merge conflict rebase main\n");
+=======
+    printf("merge conflict feature-1.\n");
+>>>>>>> 0a83b57 ([T6] editted program.c feature-1)
 }
